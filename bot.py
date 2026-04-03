0aa296ac1c6c408c9ad8da0b8bba1b3e4bdea34c
zerodev6 <zerodev699@gmail.com>
2026-04-03 16:19:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from pytz import timezone
 from pyrogram import Client, __version__
@@ -31,9 +32,11 @@ class Bot(Client):
         self.username = me.username  
         self.uptime = Config.BOT_UPTIME     
         if Config.WEBHOOK:
+            port = int(os.environ.get("PORT", 8080))
             app = web.AppRunner(await web_server())
             await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
+            await web.TCPSite(app, "0.0.0.0", port).start()
+            print(f"Web server started on port {port}")
         print(f"{me.first_name} Is Started.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**{me.first_name}  Is Started...**")                                
@@ -49,10 +52,6 @@ class Bot(Client):
                 print("Please Make This Is Admin In Your Log Channel")
 
 Bot().run()
-
-
-
-
 
 
 # Jishu Developer 
